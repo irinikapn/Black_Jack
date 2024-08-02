0ae7a83c08327df597a2f6ad04f2cf093ddba699
irinikapn <irinikapn@gmail.com>
2024-08-02 17:02:18 +0300

    get card
    Co-Authored-By: Nikita <69430978+naygoz@users.noreply.github.com>

--- bj.py
+++ bj.py
@@ -1,10 +1,30 @@
-from flask import Flask
+from flask import Flask, redirect, render_template
+from datetime import datetime as dt
+import json
+import requests
 
 app = Flask(__name__)
+global deck
+#new deck
+#вне маршрута, тк базовая функция
+def get_deck():
+    deck_id =json.loads( requests.post('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1').text)['deck_id']
+    return deck_id
+
+
 
 @app.route('/')
-def hello():
-    return 'Hello, DOT!'
+#когда человек запустил сервис, отрисовываем стартовую страницу
+def start_page():
+    deck=get_deck()
+    return render_template('index.html', deck_id=deck)
+    
+@app.route('/<deck_id>/new_card')
+def get_card(deck_id, cards_cnt='1'):
+    card = json.loads(requests.post('https://deckofcardsapi.com/api/deck/'+deck_id+'/draw/?count='+cards_cnt).text)['cards']
+    return render_template('index.html', card=card, deck_id=deck)
+
+
 
 if __name__ == '__main__':
     app.run()
